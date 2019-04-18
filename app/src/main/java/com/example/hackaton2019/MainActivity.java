281081737c31bf99dd52f6f17f2f210cb17eb1f7
jeremy <jganderatz@gmail.com>
2019-04-18 17:01:58 +0200

    music

--- app/src/main/java/com/example/hackaton2019/MainActivity.java
+++ app/src/main/java/com/example/hackaton2019/MainActivity.java
@@ -1,26 +1,43 @@
 package com.example.hackaton2019;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.mediaPlayer = new MediaPlayer();
+        String url = "https://www.palaiszelda.com/cado/mp3s_/loz/introduction.mp3";
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         Button button = findViewById(R.id.btPlay);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent message = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(message);
+
             }
         });
     }
+
 }

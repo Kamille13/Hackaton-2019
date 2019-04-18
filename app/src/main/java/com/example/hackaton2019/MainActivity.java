bde5728a55d21f39d5886d8b6344d198ae6502be
cedric <casas.cedric@gmail.com>
2019-04-18 16:23:44 +0200

    récupération de l'API character

--- app/src/main/java/com/example/hackaton2019/MainActivity.java
+++ app/src/main/java/com/example/hackaton2019/MainActivity.java
@@ -13,11 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this , ImportApi.class);
+                Intent intent = new Intent(MainActivity.this , ImportApiCharacters.class);
                 startActivity(intent);
             }
         });

973efcdce7bfcdd47eb60f8c4c6f7e6a86212c0e
Camille Brunet <camille.brunet.07@hotmail.fr>
2019-04-18 21:53:12 +0200

    Enigmes

--- app/src/main/java/com/example/hackaton2019/Enigme1Activity.java
+++ app/src/main/java/com/example/hackaton2019/Enigme1Activity.java
@@ -1,10 +1,9 @@
 package com.example.hackaton2019;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;

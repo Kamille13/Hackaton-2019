dbbc17e3dfb612dff3b4527983600e35e278a87c
cedric <casas.cedric@gmail.com>
2019-04-18 14:13:48 +0200

    xtraction eggs

---
+++ app/src/main/java/com/example/hackaton2019/ImportApi.java
@@ -0,0 +1,67 @@
+package com.example.hackaton2019;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ImportApi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_api);
+
+        // Crée une file d'attente pour les requêtes vers l'API
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        // TODO : URL de la requête vers l'API
+        String url = "http://easteregg.wildcodeschool.fr/api/eggs";
+
+        // Création de la requête vers l'API, ajout des écouteurs pour les réponses et erreurs possibles
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray list) {
+                        TextView tvName = findViewById(R.id.tvName);
+                        try {
+                            for(int i = 0; i < list.length(); i++){
+                                JSONObject eggs = list.getJSONObject(i);
+                                String name = eggs.getString("name");
+                                tvName.append(name);
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Afficher l'erreur
+                        Log.d("VOLLEY_ERROR", "onErrorResponse: " + error.getMessage());
+                    }
+                }
+        );
+
+        // On ajoute la requête à la file d'attente
+        requestQueue.add(jsonArrayRequest);
+    }
+}

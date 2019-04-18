ec56b3fb9eb6c236e5082ec23de00424cf3791ab
cedric <casas.cedric@gmail.com>
2019-04-18 14:57:03 +0200

    ajout de la liste egg

--- app/src/main/java/com/example/hackaton2019/ImportApi.java
+++ app/src/main/java/com/example/hackaton2019/ImportApi.java
@@ -38,11 +38,69 @@ public class ImportApi extends AppCompatActivity {
                     public void onResponse(JSONArray list) {
                         TextView tvName = findViewById(R.id.tvName);
                         try {
-                            for(int i = 0; i < list.length(); i++){
-                                JSONObject eggs = list.getJSONObject(i);
-                                String name = eggs.getString("name");
-                                tvName.append(name);
-                            }
+                                JSONObject eggs = list.getJSONObject(0);
+                                String eggOfTuna = eggs.getString("name");
+                                String rarityegg = eggs.getString("rarity");
+                                tvName.append(eggOfTuna);
+
+                                JSONObject egg2 = list.getJSONObject(1);
+                                String eggOfFlounderWinter = egg2.getString("name");
+                                String rarityegg2 = eggs.getString("rarity");
+                                tvName.append(eggOfFlounderWinter);
+
+                                JSONObject egg3 = list.getJSONObject(8);
+                                String eggOfKiwi = egg3.getString("name");
+                                String rarityegg3 = eggs.getString("rarity");
+                                tvName.append(eggOfKiwi);
+
+                                JSONObject egg4 = list.getJSONObject(11);
+                                String rarityegg4 = eggs.getString("rarity");
+                                String eggOfChameleon = egg4.getString("name");
+                                tvName.append(eggOfChameleon);
+
+                                JSONObject egg5 = list.getJSONObject(36);
+                                String rarityegg5 = eggs.getString("rarity");
+                                String eggOfSturgeonAtlantic = egg5.getString("name");
+                                tvName.append(eggOfSturgeonAtlantic);
+
+                                JSONObject egg6 = list.getJSONObject(37);
+                                String rarityegg6 = eggs.getString("rarity");
+                                String eggOfFrogBull = egg6.getString("name");
+                                tvName.append(eggOfFrogBull);
+
+                                JSONObject egg7 = list.getJSONObject(38);
+                                String rarityegg7 = eggs.getString("rarity");
+                                String eggOfOstrich = egg7.getString("name");
+                                tvName.append(eggOfOstrich);
+
+                                JSONObject egg8 = list.getJSONObject(40);
+                                String rarityegg8 = eggs.getString("rarity");
+                                String eggOfEelAmerican = egg8.getString("name");
+                                tvName.append(eggOfEelAmerican);
+
+                                JSONObject egg9 = list.getJSONObject(48);
+                                String rarityegg9 = eggs.getString("rarity");
+                                String eggOfFrogleopard = egg9.getString("name");
+                                tvName.append(eggOfFrogleopard);
+
+                                JSONObject egg10 = list.getJSONObject(51);
+                                String rarityegg10 = eggs.getString("rarity");
+                                String eggOfPokemon = egg10.getString("name");
+                                tvName.append(eggOfPokemon);
+
+                                String[] [] eggsArray = new String [][]{
+                                        {eggOfTuna , rarityegg },
+                                        {eggOfFlounderWinter , rarityegg2},
+                                        {eggOfKiwi , rarityegg3},
+                                        {eggOfChameleon,rarityegg4},
+                                        {eggOfSturgeonAtlantic,rarityegg5},
+                                        {eggOfFrogBull,rarityegg6},
+                                        {eggOfOstrich,rarityegg7},
+                                        {eggOfEelAmerican,rarityegg8},
+                                        {eggOfFrogleopard,rarityegg9},
+                                        {eggOfPokemon,rarityegg10},
+
+                                };
 
 
                         } catch (JSONException e) {

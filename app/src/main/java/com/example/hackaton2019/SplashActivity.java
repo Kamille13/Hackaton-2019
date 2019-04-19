83f5a02ce95e5681f5e7b895ea520ecf0824f5af
cedric <casas.cedric@gmail.com>
2019-04-19 11:43:29 +0200

    with splashActivity

---
+++ app/src/main/java/com/example/hackaton2019/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.hackaton2019;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread myThread = new Thread() {
+
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        myThread.start();
+    }
+}
